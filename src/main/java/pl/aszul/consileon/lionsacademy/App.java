795dd8559a4d474755ebb10c2a499c273a4abe27
Andrzej Szul <34945957+zerg-rush@users.noreply.github.com>
2018-10-18 10:40:44 +0200

    fresh project template

---
+++ src/main/java/pl/aszul/consileon/lionsacademy/App.java
@@ -0,0 +1,13 @@
+package pl.aszul.consileon.lionsacademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pl/aszul/consileon/lionsacademy/AppTest.java
@@ -0,0 +1,20 @@
+package pl.aszul.consileon.lionsacademy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
